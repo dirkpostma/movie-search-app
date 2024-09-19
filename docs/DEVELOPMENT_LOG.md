332dd02678662350eb10abf14ea04febfa7d1362
Dirk Postma <d.postma@alfen.com>
2024-09-19 12:34:20 +0200

    chore: add @testing-library/react-native

---
+++ docs/DEVELOPMENT_LOG.md
@@ -0,0 +1,6 @@
+# Development log Movie Search App
+
+## deprecated renderer.create
+- renderer.create from react-test-renderer is deprecated
+- added @testing-library/react-native
+Reference: https://react.dev/warnings/react-test-renderer
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,11 @@
 import 'react-native';
 import React from 'react';
 import {App} from './App';
+import {describe, it} from '@jest/globals';
+import {render} from '@testing-library/react-native';
 
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('App', () => {
+  it('renders correctly', () => {
+    render(<App />);
+  });
 });
