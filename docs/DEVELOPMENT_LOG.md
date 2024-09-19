5276e0898838d10e6ae3b00fa8f70eb384d8d8ad
Dirk Postma <d.postma@alfen.com>
2024-09-19 14:14:31 +0200

    feat: setup redux toolkit and add counter example

--- docs/DEVELOPMENT_LOG.md
+++ docs/DEVELOPMENT_LOG.md
@@ -4,3 +4,8 @@
 - renderer.create from react-test-renderer is deprecated
 - added @testing-library/react-native
 Reference: https://react.dev/warnings/react-test-renderer
+
+## Setup redux toolkit
+
+Followed guide on https://redux-toolkit.js.org/tutorials/quick-start
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import { store } from './core/store/store';
+import {Provider} from 'react-redux';
+import { Counter } from './features/counter/counter';
 
 export const App = () => {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View>
-          <Text>Movie Search App</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <Provider store={store}>
+      <SafeAreaView>
+        <ScrollView>
+          <View>
+            <Text>Movie Search App</Text>
+            <Counter />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </Provider>
   );
 };
