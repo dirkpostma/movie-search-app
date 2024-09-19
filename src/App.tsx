dd0336fb097263904737408cbfc3fb121e570492
Dirk Postma <d.postma@alfen.com>
2024-09-19 12:28:52 +0200

    chore: install pods and cleanup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+
+export const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>Movie Search App</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
